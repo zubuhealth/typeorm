1b34c9a49e99fa937dd9894ddb7e6bba85c08a42
sagentac <128644336+sagentac@users.noreply.github.com>
2024-01-26 06:30:40 +0100

    fix: multiple insert in SAP Hana (#10597)
    Co-authored-by: Philipp Haeusle <>

--- src/query-builder/InsertQueryBuilder.ts
+++ src/query-builder/InsertQueryBuilder.ts
@@ -459,7 +459,10 @@ export class InsertQueryBuilder<
         // add VALUES expression
         if (valuesExpression) {
             if (
-                this.connection.driver.options.type === "oracle" &&
+                (
+                    this.connection.driver.options.type === "oracle" ||
+                    this.connection.driver.options.type === "sap"
+                ) &&
                 this.getValueSets().length > 1
             ) {
                 query += ` ${valuesExpression}`
